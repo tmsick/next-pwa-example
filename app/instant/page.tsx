e65e78e1cca13d717a04a2daa2e1f6ff83884d94
Tomoya Yoshioka <tomoya.y@icloud.com>
2024-11-22 02:12:54 +0900

    Update

---
+++ app/instant/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useFcmToken } from "@/hooks/use-fcm"
+
+const Page = () => {
+  const { getToken, isSupported, permissionState } = useFcmToken()
+
+  const [token, setToken] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    console.log("isSupported", isSupported)
+    console.log("permissionState", permissionState)
+    console.log("token", token)
+    console.log("--")
+    ;(async () => {
+      setLoading(true)
+
+      if (!getToken) {
+        setLoading(false)
+        return
+      }
+
+      try {
+        setToken(await getToken())
+      } catch (error) {
+        console.warn(error)
+      }
+      setLoading(false)
+    })()
+  }, [getToken, isSupported, permissionState, token])
+
+  return (
+    <div>
+      {loading && <p>Loading...</p>}
+      {!!token && <p>Token: {token}</p>}
+      {!loading && permissionState === "denied" && <p>Permission denied 😢</p>}
+      {!loading && permissionState === "prompt" && <p>Permission prompt 🤔</p>}
+    </div>
+  )
+}
+
+export default Page
